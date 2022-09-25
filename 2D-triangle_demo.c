82d063727706afd0b56f5047aafefd11fc1c0baf
0x15c <68035487+0x15c@users.noreply.github.com>
2022-09-25 15:15:47 +0800

    Add files via upload
    Add optional fps support

--- 2D-triangle_demo.c
+++ 2D-triangle_demo.c
@@ -11,12 +11,14 @@
  */
 #include<stdio.h>
 #include<math.h>
+#include<stdlib.h>
 #include<windows.h>
+#include<string.h>
 #define CON_COLS 80//set by hand in windows terminal
 #define CON_ROWS 30
 #define PI 3.1416
 #define LINE_CRIT -0.99
-#define FRESH_RATE 10
+#define DEFAULT_FRESH_RATE 5
 typedef struct vector2D
 {
     float x;
@@ -46,15 +48,20 @@ int draw_vldt(dline_vldt *input);//evaluating if a pixel shoubld be filled
 float dist_eval(vector2D ptA, vector2D ptB);
 void align_coord(pixel2D *to_be_ali, vector2D *aligned);//align screen coordination into real plane
 
-int main()
+int main(int* argc, char* argv[1])//optional arguments support, you can set your own fps(just for fun).
 {
+    int refresh_rate = DEFAULT_FRESH_RATE;
+    if (argv[1] != NULL)
+    {
+        refresh_rate = atoi(argv[1]);
+    }
     pixel2D plain_pt={0,0};//the point need to be validate, pixel by pixel
     vector2D coord_pt={0.0f,0.0f};
     float alpha = PI/90;
     delta test_triangle={
         .ptA = {-0.2f , -0.2f},
-        .ptB = {0.2f  , 0.2f},
-        .ptC = {0.2f  , -0.2f},
+        .ptB = {-0.2f , 0.2f},
+        .ptC = {0.0f  , 0.3f},
     };
     dline_vldt validate;
     int crit=0;
@@ -64,9 +71,9 @@ int main()
         coord_rotate(&test_triangle.ptB,alpha);
         coord_rotate(&test_triangle.ptC,alpha);
         //debug info
-        printf("ptA=[%f,%f],ptB=[%f,%f],ptC=[%f,%f]\n",test_triangle.ptA.x,test_triangle.ptA.y,test_triangle.ptB.x,test_triangle.ptB.y,test_triangle.ptC.x,test_triangle.ptC.y);
+        printf("fps=%d,ptA=(%3.2f,%3.2f),ptB=(%3.2f,%3.2f),ptC=(%3.2f,%3.2f)\n",refresh_rate,test_triangle.ptA.x,test_triangle.ptA.y,test_triangle.ptB.x,test_triangle.ptB.y,test_triangle.ptC.x,test_triangle.ptC.y);
             
-        //frame drawing begin, scanning from left top to right bottom
+        //fream drawing begin, scanning from left top to right bottom
         for (plain_pt.y=1;plain_pt.y<=CON_ROWS-1;plain_pt.y++)
         {
             for(plain_pt.x=1;plain_pt.x<=CON_COLS;plain_pt.x++)
@@ -89,7 +96,7 @@ int main()
             }
         }
         //end drawing
-        Sleep(1000/FRESH_RATE);//Throushold
+        Sleep(1000/refresh_rate);//Throushold
     }
 
 }
