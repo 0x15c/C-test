1d660c752847541d983db19271435b937a097b0b
0x15c <68035487+0x15c@users.noreply.github.com>
2022-10-05 21:56:51 +0800

    加入了矩阵乘法的例子

--- DMatrix/MatExample.c
+++ DMatrix/MatExample.c
@@ -11,20 +11,34 @@
 #include <stdio.h>
 #include "Dmatrix.h"
 #include "DMatrix.c"
+#ifndef Dtype
+    #define Dtype float
+    /**
+     * @brief for universal usage, replace float with double, uint32, ...
+     * already defined in "DMatrix.h"
+     */
+#endif
 int main()
 {
-    float test_mat[3][4] = {
+    Dtype test_mat[2][4] = {
         {2.0f,4.0f ,8.0f  ,16.0f  },
-        {0.5f,0.25f,0.125f,0.0625f},
-        {6.0f,10.0f,12.0f ,14.0f  },// those are "integers" for float32 type.
+        {0.5f,0.25f,0.125f,0.0625f},// those are "integers" for float32 type.
     };
     Matrix test={
         .col=4,
-        .row=3,//must be declared by hand
+        .row=2,//must be declared by hand
         .mat_index=&test_mat[0][0],// set the pointer on the first entry of matrix
     };
     DMprint(&test);
-    DMtrans(&test);
-    DMprint(&test);
+    Matrix trans = DMtrans(&test);
+    printf("\n");
+    DMprint(&trans);
+    Matrix ans = DMmultiply(&trans,&test);
+    printf("\n");
+    DMprint(&ans);
+
+
+    free(trans.mat_index);
+    free(ans.mat_index);
     return 0;
 }
