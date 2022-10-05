20ce89618937c877adbe28af5bd8d33cf0d0a519
0x15c <68035487+0x15c@users.noreply.github.com>
2022-10-05 21:54:59 +0800

    避重就轻，先把矩阵乘法实现了（三重嵌套循环）

--- DMatrix/Dmatrix.c
+++ DMatrix/Dmatrix.c
@@ -11,28 +11,54 @@
  * 
  */
 #include "Dmatrix.h"
-void DMtrans(Matrix *tMat)
+Matrix DMtrans(Matrix *tMat)
 {
     int i,j,tmp=0;
     int block_size = tMat->col*tMat->row;
-    float *mat_index = NULL;
-    mat_index = (float*)malloc(sizeof(float)*block_size);
+    Matrix mat_ans;
+    mat_ans.mat_index = (Dtype*)malloc(sizeof(Dtype)*block_size);
     //ask for allocated mem space
     for(i=0;i<tMat->row;i++)
     {
         for(j=0;j<tMat->col;j++)
         {
-            mat_index[i+j*tMat->row]=tMat->mat_index[j+i*tMat->col];
+            mat_ans.mat_index[i+j*tMat->row]=tMat->mat_index[j+i*tMat->col];
         }// temp_matrix[j,i]=target_matrix[i,j]
     }
-    for(i=0;i<block_size;i++)
+    mat_ans.row=tMat->col;
+    mat_ans.col=tMat->row;
+    return mat_ans;
+    
+}
+Matrix DMmultiply(Matrix *s1Mat, Matrix *s2Mat)
+{
+    int i,j,k,l,p=0;
+    Matrix DMret;
+    DMret.mat_index=NULL;
+    if ((p = s1Mat->col) != s2Mat->row)
+    return DMret; //check if the operation is legal, if not, return a NULL.
+
+    int block_size = s1Mat->row*s2Mat->col;
+    //Dtype *mat_index = NULL;
+    //mat_index = (Dtype*)malloc(sizeof(Dtype)*block_size);
+    
+    DMret.row=s1Mat->row;
+    DMret.col=s2Mat->col;
+
+    DMret.mat_index = (Dtype*)malloc(sizeof(Dtype)*block_size);
+    for(i=0;i<DMret.row;i++)
     {
-        tMat->mat_index[i]=mat_index[i];
-    }// set target equal to temp
-    free(mat_index);//free mem space
-    tmp = tMat->col;
-    tMat->col = tMat->row;
-    tMat->row = tmp;//reset the info of columns and rows
+        for(j=0;j<DMret.col;j++)
+        {
+            DMret.mat_index[i*DMret.row+j]=0;
+
+            for(k=0;k<p;k++)
+            {  
+                DMret.mat_index[i*DMret.row+j]+=s1Mat->mat_index[i*s1Mat->col+k] * s2Mat->mat_index[k*s2Mat->col+j];
+            }
+        }
+    }
+    return DMret;
 }
 void DMprint(Matrix *tMat)
 {// a straightforward print function
